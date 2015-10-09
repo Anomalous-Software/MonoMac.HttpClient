8c663ec9b5fcd7003e32d339acef5e49bc30771b
Andrew Piper <threax@users.noreply.github.com>
2015-10-09 19:37:42 +0000

    Attempting to create our own backend for httpclient using the mono mac bindings.
    git-svn-id: https://192.168.3.139:8443/svn/ArticDependencies/trunk/Mono/OSXBuild/MonoMac.HttpClient@760 be14db4f-12c9-2748-9cb2-092a125f2d7f

---
+++ NativeMessageConnectionDelegate.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using MonoMac.Foundation;
+using System.Threading;
+
+namespace MonoMac.HttpClient
+{
+    class NativeMessageConnectionDelegate : NSUrlConnectionDelegate
+    {
+        private NativeMessageHandler handler;
+        private ManualResetEventSlim waitEvent = new ManualResetEventSlim(false);
+
+        public NativeMessageConnectionDelegate(NativeMessageHandler handler)
+        {
+            this.handler = handler;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            waitEvent.Dispose();
+            base.Dispose(disposing);
+        }
+
+        public override void ReceivedResponse(NSUrlConnection connection, NSUrlResponse response)
+        {
+            Console.WriteLine("received");
+            //throw new System.NotImplementedException ();
+            //waitEvent.Set();
+        }
+
+        public override void FailedWithError(NSUrlConnection connection, NSError error)
+        {
+            waitEvent.Set();
+        }
+
+        public override void FinishedLoading(NSUrlConnection connection)
+        {
+            waitEvent.Set();
+        }
+
+        public void wait()
+        {
+            waitEvent.Wait();
+        }
+    }
+}
+
---
+++ NativeMessageHandler.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using MonoMac.Foundation;
+
+namespace MonoMac.HttpClient
+{
+    public class NativeMessageHandler : HttpClientHandler
+    {
+        public NativeMessageHandler()
+        {
+        }
+
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            return Task.Run(() =>
+                {
+                    HttpResponseMessage response = new HttpResponseMessage();
+                    response.StatusCode = System.Net.HttpStatusCode.NotFound;
+                    using(NSUrl url = new NSUrl(request.RequestUri.GetComponents(UriComponents.AbsoluteUri, UriFormat.UriEscaped)))
+                    {
+                        using(NSUrlRequest nsRequest = new NSUrlRequest(url, MonoMac.Foundation.NSUrlRequestCachePolicy.ReloadIgnoringCacheData, 60))
+                        {
+                            NSUrlResponse nsResponse;
+                            NSError nsError;
+
+                            NSData data = NSUrlConnection.SendSynchronousRequest(nsRequest, out nsResponse, out nsError);
+
+                            NSHttpUrlResponse nsHttpResponse = nsResponse as NSHttpUrlResponse;
+                            if(nsHttpResponse != null)
+                            {
+                                foreach(var header in nsHttpResponse.AllHeaderFields)
+                                {
+                                    if(header.Key != null && header.Value != null)
+                                    {
+                                        response.Headers.TryAddWithoutValidation(header.Key.ToString(), header.Value.ToString());
+                                        response.Content.Headers.TryAddWithoutValidation(header.Key.ToString(), header.Value.ToString());
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    return response;
+                });
+        }
+    }
+}
+
