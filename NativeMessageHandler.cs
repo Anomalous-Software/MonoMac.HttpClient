318e19c9fe0923e8b59a79a3eedabd71d1881f6a
Andrew Piper <threax@users.noreply.github.com>
2015-10-13 14:32:20 +0000

    Actually set the request body, this works at least enough to log in now.
    git-svn-id: https://192.168.3.139:8443/svn/ArticDependencies/trunk/Mono/OSXBuild/MonoMac.HttpClient@769 be14db4f-12c9-2748-9cb2-092a125f2d7f

--- NativeMessageHandler.cs
+++ NativeMessageHandler.cs
@@ -40,6 +40,7 @@ namespace MonoMac.HttpClient
                         var rq = new NSMutableUrlRequest()
                         {
                             AllowsCellularAccess = true,
+                            Body = NSData.FromArray(ms.ToArray()),
                             CachePolicy = NSUrlRequestCachePolicy.ReloadIgnoringCacheData,
                             Headers = headers.Aggregate(new NSMutableDictionary(), (acc, x) =>
                                 {
@@ -60,44 +61,6 @@ namespace MonoMac.HttpClient
 
                         return connectionDelegate.Response;
                     }
-                });
-
-            return Task.Run(() =>
-                {
-                    HttpResponseMessage response = new HttpResponseMessage();
-                    response.StatusCode = System.Net.HttpStatusCode.NotFound;
-                    using (NSUrl url = new NSUrl(request.RequestUri.GetComponents(UriComponents.AbsoluteUri, UriFormat.UriEscaped)))
-                    {
-                        using (NSUrlRequest nsRequest = new NSUrlRequest(url, MonoMac.Foundation.NSUrlRequestCachePolicy.ReloadIgnoringCacheData, 60))
-                        {
-                            NSUrlResponse nsResponse;
-                            NSError nsError;
-
-                            NSData data = NSUrlConnection.SendSynchronousRequest(nsRequest, out nsResponse, out nsError);
-
-                            if (data != null)
-                            {
-                                NSHttpUrlResponse nsHttpResponse = nsResponse as NSHttpUrlResponse;
-                                if (nsHttpResponse != null)
-                                {
-                                    response.StatusCode = System.Net.HttpStatusCode.OK;
-                                    foreach (var header in nsHttpResponse.AllHeaderFields)
-                                    {
-                                        if (header.Key != null && header.Value != null)
-                                        {
-                                            response.Headers.TryAddWithoutValidation(header.Key.ToString(), header.Value.ToString());
-                                            //response.Content.Headers.TryAddWithoutValidation(header.Key.ToString(), header.Value.ToString());
-                                        }
-                                    }
-                                }
-                            }
-                            else
-                            {
-                                
-                            }
-                        }
-                    }
-                    return response;
                 });
         }
 
